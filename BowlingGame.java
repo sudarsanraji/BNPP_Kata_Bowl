0c93453d6973325c0bdf14e103bc5068bfd9d9fd
sudarsanraji <111418105+sudarsanraji@users.noreply.github.com>
2022-10-31 17:23:05 +0530

    Add files via upload
    Java and test classes created and created methods.

---
+++ BowlingGame.java
@@ -0,0 +1,11 @@
+package bnp.kata.bowl;
+
+public class BowlingGame {
+public void bowl(int sticksDown){
+	
+}
+public int score(){
+	return 0;
+}
+
+}
---
+++ BowlingGameTest.java
@@ -0,0 +1,23 @@
+package bnp.kata.bowl;
+
+import org.junit.*;
+
+public class BowlingGameTest  {
+@Test
+private void startGame() {
+	// TODO Auto-generated method stub
+new BowlingGame();
+
+BowlingGame bg = new BowlingGame();
+bg.bowl(0);
+
+}
+
+@Test
+private void gamescore() {
+	// TODO Auto-generated method stub
+new BowlingGame();
+
+}
+
+}
